fd855acbf46d16982ba1fb58986ea579f8e16eec
dnjiao <oscarjiao@gmail.com>
2017-06-25 22:40:50 -0500

    Update tree methods

--- src/tree/PrintDistance.java
+++ src/tree/PrintDistance.java
@@ -29,25 +29,30 @@ public class PrintDistance {
 		return 0;
 	}
 
-	public Node findLCA(Node root, int n1, int n2) {
-		if (root != null) {
-			if (root.data == n1 || root.data == n2) {
-				return root;
-			}
-			Node left = findLCA(root.left, n1, n2);
-			Node right = findLCA(root.right, n1, n2);
-
-			if (left != null && right != null) {
-				return root;
-			}
-			if (left != null) {
-				return left;
-			}
-			if (right != null) {
-				return right;
-			}
-		}
-		return null;
+	public Node findLCA(Node node, int n1, int n2) {
+		
+		// Base case
+        if (node == null)
+            return null;
+ 
+        // If either n1 or n2 matches with root's key, report
+        // the presence by returning root (Note that if a key is
+        // ancestor of other, then the ancestor key becomes LCA
+        if (node.data == n1 || node.data == n2)
+            return node;
+ 
+        // Look for keys in left and right subtrees
+        Node left_lca = findLCA(node.left, n1, n2);
+        Node right_lca = findLCA(node.right, n1, n2);
+ 
+        // If both of the above calls return Non-NULL, then one key
+        // is present in once subtree and other is present in other,
+        // So this node is the LCA
+        if (left_lca!=null && right_lca!=null)
+            return node;
+ 
+        // Otherwise check if left subtree or right subtree is LCA
+        return (left_lca != null) ? left_lca : right_lca;
 	}
 
 	public static void main(String[] args) throws java.lang.Exception {
